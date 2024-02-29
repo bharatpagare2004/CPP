e3aa06293f292044e38bacc4d26d47683a8e48bc
bharatpagare2004 <bharatpagare018@gmail.com>
2024-02-29 23:22:58 +0530

    size

---
+++ simple.cpp
@@ -0,0 +1,17 @@
+// finding the size of variable...
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"size of char: "<<sizeof(char)<<" byte"<<endl;
+    cout<<"size of int: "<<sizeof(int)<<" bytes"<<endl;
+    cout<<"size of float: "<<sizeof(float)<<" bytes"<<endl;
+    cout<<"size of double: "<<sizeof(double)<<" bytes"<<endl;
+    cout<<"size of short: "<<sizeof(short)<<" bytes"<<endl;
+    cout<<"size of long: "<<sizeof(long)<<" bytes"<<endl;
+    cout<<"size of bool: "<<sizeof(bool)<<" bytes"<<endl;
+    
+    return 0;
+
+}
