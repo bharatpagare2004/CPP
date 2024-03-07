c8328f6feefc973c85a2cf289bbe452ccc2e6af0
bharatpagare2004 <bharatpagare018@gmail.com>
2024-03-07 22:50:45 +0530

    to

---
+++ check_flow.cpp
@@ -0,0 +1,13 @@
+// this program for checking the flow of range 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1 = 2147483647; 
+    cout<<n1+1<<endl;
+    cout<<n1+2<<endl;
+
+    int n2 = -2147483647;
+    cout<<n2-1<<endl;
+    cout<<n2-2<<endl;
+}
---
+++ increament.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n = 10;
+    cout<<"the number is:"<<n<<endl;
+    n++;
+    cout<<"the number is after increament:"<<n<<endl;
+    int t = 90;
+    t--;
+    cout<<"the number is after decreament:"<<t<<endl;
+}
