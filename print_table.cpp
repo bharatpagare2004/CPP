9852ba416c7210f29919bad5c79c0f7267d367b9
bharatpagare2004 <bharatpagare018@gmail.com>
2024-04-11 18:29:23 +0530

    table

---
+++ print_table.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n ;
+    cout<<"\nenter the number you want to enter:";
+    cin>>n;
+    for(int i = 1;i<=10;i++)
+    {
+        cout<<i*n<<endl;
+
+    }
+}
